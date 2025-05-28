be5a35f59ee646fafc348312ace959eb2345716d
Towa <soccertowa0923@outlook.com>
2025-05-28 20:56:03 +0900

    Fix: ensure PORT fallback and correct config format

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    host:'0.0.0.0',
+   port: parseInt(process.env.PORT ? process.env.PORT : '5173'),
+  },
   plugins: [react(),
     tailwindcss(),
   ],
