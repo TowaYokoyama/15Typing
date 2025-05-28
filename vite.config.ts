0c1271290b6d8b4924b3eb9c06b58f57150aea2c
Towa <soccertowa0923@outlook.com>
2025-05-28 21:06:21 +0900

    last commitment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   server:{
     host:'0.0.0.0',
    port: parseInt(process.env.PORT ? process.env.PORT : '5173'),
+       allowedHosts: ['one5typing.onrender.com'],  // ここにホスト名を追加
   },
   plugins: [react(),
     tailwindcss(),
